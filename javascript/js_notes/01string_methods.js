02662106a4f70d274fe934a4f94b43c456af0066
Matt Frutchey <mjfru201@gmail.com>
2024-11-24 13:40:30 -0500

    add brief string method note page

---
+++ javascript/js_notes/01string_methods.js
@@ -0,0 +1,10 @@
+//! Slice - slice(x, y);
+
+const firstName = "Matt";
+firstName.slice(0, 1); // Taking a 'slice' out of firstName starting at 0 and up to, not including, 1.
+firstName.slice(0, 3) // 'Mat'
+//? Do some basic subtraction to figure out how many characters you'll end up with.
+
+//! ToUpperCase - string.toUpperCase()
+let myName = "Matt"
+upperCaseName = myName.toUpperCase();
