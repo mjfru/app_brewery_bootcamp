8df71e9fb61f46f8a8205c14c941cd415519267b
Matt Frutchey <mjfru201@gmail.com>
2024-12-05 08:33:40 -0500

    finish simple dice game for js concepts

---
+++ javascript/js_exercises/dice_game/script.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+console.log(randomNumber1);
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏁 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🏁";
+} else {
+  document.querySelector("h1").innerHTML = "Draw! ⛔ Try Again.";
+}
