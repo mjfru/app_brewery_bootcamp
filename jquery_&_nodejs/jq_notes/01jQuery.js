523098955465d8722f030dfc192b5e3d63647a42
Matt Frutchey <mjfru201@gmail.com>
2024-12-06 06:56:15 -0500

    add note summary on jQuery

---
+++ jquery_&_nodejs/jq_notes/01jQuery.js
@@ -0,0 +1,12 @@
+/*
+! What is jQuery?
+* jQuery is a JS library that is widely used but slowly becoming antiquated.
+It takes a lot of the lengthy aspects of JavaScript and simplifies it quite a bit.
+
+document.querySelector("h1") becomes jQuery("h1")...or $("h1")
+
+*jQuery can be incorporated by using CDN, and Google's is the most popular.
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+* The positioning of this script is important-- make sure it's above your own personal scripts at the end of the page.
+*/
