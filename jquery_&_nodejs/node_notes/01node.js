b82a75e9acb40e9beaf7308e08a5f1070d821474
Matt Frutchey <mjfru201@gmail.com>
2024-12-13 06:18:16 -0500

    add very brief node notes, most on the REPL and NPM

---
+++ jquery_&_nodejs/node_notes/01node.js
@@ -0,0 +1,11 @@
+/*
+! Using the REPL:
+* (Read Eval Print Loop)
+- A computer environment where user inputs are read and evaluated and then the results are returned to the user (similar to writing JS directly in the browser dev tools)
+
+* The 'node' command enters you into the REPL from the command line.
+
+The native modules Node.js comes with can be thought of as a toolbox we already have at home to let us create our web apps.
+
+NPM is like shopping for specific tools that others have made and are sharing with other developers.
+*/
